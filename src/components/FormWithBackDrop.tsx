7d0ee8a0b0f54f38cfa29bd6a066863b3bfc54f0
giangann <70595717+giangann@users.noreply.github.com>
2022-10-24 20:07:26 +0700

    remove node_module from git

--- src/components/FormWithBackDrop.tsx
+++ src/components/FormWithBackDrop.tsx
@@ -2,7 +2,7 @@ import { Button, Typography } from "@mui/material";
 import Backdrop from "@mui/material/Backdrop";
 import React from "react";
 import { CustomForm } from "./CustomForm";
-
+import AcUnitIcon from '@mui/icons-material/AcUnit';
 export const FormWithBackDrop = () => {
   const [open, setOpen] = React.useState(false);
   const handleClose = () => {
@@ -14,6 +14,7 @@ export const FormWithBackDrop = () => {
 
   return (
     <React.Fragment>
+      <AcUnitIcon/>
       <Button onClick={handleToggle}>Toggle backdrop</Button>
       <Backdrop
         sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}

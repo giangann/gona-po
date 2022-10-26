05dfef7a2f7c4bef5a5218d7a398b899c71ef4b8
giangann <70595717+giangann@users.noreply.github.com>
2022-10-26 11:47:40 +0700

    [Refactor: useInview instead of react-fullpage] - []

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,6 +1,18 @@
 export const imageSlideHomePage = [
-    "https://eqx-assets-prod.imgix.net/files/4cc6dd7c8e024a50a3c92bcad02e276d-victor-xok-jxmmltiqpk8-unsplash.jpg?auto=format&fit=crop&ixlib=react-8.6.4&w=1946",
-    "https://eqx-assets-prod.imgix.net/images/ca7c4f87aef6432f9f902af5a921ef64-costa-rica-hero-image.jpg?auto=format&fit=crop&ixlib=react-8.6.4&w=1678",
-    "https://eqx-assets-prod.imgix.net/images/8d35f14cf20f4f2fa6caa1f5ac562bf5-florence-mid.jpg?auto=format&fit=crop&ixlib=react-8.6.4&w=1946",
-    "https://eqx-assets-prod.imgix.net/images/17f4590aff6d48d5ba275c8e30fa98cb-5732_lr_morocco_hero.png?auto=format&fit=crop&ixlib=react-8.6.4&w=1946"
-]
+  {
+    id: 0,
+    link: "https://eqx-assets-prod.imgix.net/files/4cc6dd7c8e024a50a3c92bcad02e276d-victor-xok-jxmmltiqpk8-unsplash.jpg?auto=format&fit=crop&ixlib=react-8.6.4&w=1946",
+  },
+  {
+    id: 1,
+    link: "https://eqx-assets-prod.imgix.net/files/4cc6dd7c8e024a50a3c92bcad02e276d-victor-xok-jxmmltiqpk8-unsplash.jpg?auto=format&fit=crop&ixlib=react-8.6.4&w=1946",
+  },
+  {
+    id: 2,
+    link: "https://eqx-assets-prod.imgix.net/images/8d35f14cf20f4f2fa6caa1f5ac562bf5-florence-mid.jpg?auto=format&fit=crop&ixlib=react-8.6.4&w=1946",
+  },
+  {
+    id: 3,
+    link: "https://eqx-assets-prod.imgix.net/images/17f4590aff6d48d5ba275c8e30fa98cb-5732_lr_morocco_hero.png?auto=format&fit=crop&ixlib=react-8.6.4&w=1946",
+  },
+];

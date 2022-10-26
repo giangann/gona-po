aec030eb4ad63c4ea12502348a551c443a5af471
giangann <70595717+giangann@users.noreply.github.com>
2022-10-26 09:58:53 +0700

    Initial slide + header

--- src/components/CustomForm.tsx
+++ src/components/CustomForm.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Stack, styled, Typography } from "@mui/material";
 import { sampleFormData } from "~/constants/form";
+import { Header } from "~/screens/Header";
 
 export const CustomForm = () => {
   const demoData = sampleFormData[0];
@@ -24,9 +25,7 @@ export const CustomForm = () => {
         sx={{ display: "flex", justifyContent: "center" }}
       >
         <FormButton>
-          <ThickTypo>
-            Trip details
-          </ThickTypo>
+          <ThickTypo>Trip details</ThickTypo>
         </FormButton>
       </Box>
     </Box>

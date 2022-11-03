46b0ac53644c12e8bf88afce77a258212226f41f
giangann <70595717+giangann@users.noreply.github.com>
2022-11-03 13:09:06 +0700

    use env

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
+// @ts-nocheck
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base:'/gona-po',
-  plugins: [react(), tsconfigPaths()],
-});
+
+export default ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    // To access env vars here use process.env.TEST_VAR
+    base: process.env.VITE_BASE_URL,
+    plugins: [react(), tsconfigPaths()],
+    define: {
+      global: {},
+      "process.env": {},
+    },
+  });
+};
